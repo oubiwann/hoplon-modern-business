3cc84c8916e78dc22adca2832974fecbdeae33b0
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2014-01-25 12:38:17 -0600

    Updated files to point at new sources.

--- project.clj
+++ project.clj
@@ -6,8 +6,9 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [http-kit "2.1.16"]
                  [ring "1.2.1"]
-                 [compojure "1.1.6"]]
-  :source-paths ["src"]
+                 [compojure "1.1.6"]
+                 [org.clojure/clojurescript "0.0-2138"]]
+  :source-paths ["src/clj"]
   :jvm-opts ^:replace ["-Xmx512m" "-server"]
   :aot [devserver]
   :main devserver/run)
